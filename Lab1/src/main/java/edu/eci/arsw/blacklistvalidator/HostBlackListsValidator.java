28c686e75c220f9a162d9c197a09db7d3075cfc1
Sebastian Ospina <juansebastian200103@gmail.com>
2022-02-07 16:16:51 -0500

    empalme laboratorio un o completo usando variables atomicas

---
+++ Lab1/src/main/java/edu/eci/arsw/blacklistvalidator/HostBlackListsValidator.java
@@ -0,0 +1,127 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.java.edu.eci.arsw.blacklistvalidator;
+
+import main.java.edu.eci.arsw.spamkeywordsdatasource.HostBlacklistsDataSourceFacade;
+import java.util.LinkedList;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.concurrent.atomic.*;
+
+/**
+ *
+ * @author hcadavid
+ */
+public class HostBlackListsValidator {
+
+
+    private static final int BLACK_LIST_ALARM_COUNT=5;
+     private AtomicInteger ocurrencesCount=new AtomicInteger(0);
+     private List<Integer> blackListOcurrences= new LinkedList<>();
+     private AtomicInteger checkedListsCount= new AtomicInteger(0);
+	
+    
+    /**
+     * Check the given host's IP address in all the available black lists,
+     * and report it as NOT Trustworthy when such IP was reported in at least
+     * BLACK_LIST_ALARM_COUNT lists, or as Trustworthy in any other case.
+     * The search is not exhaustive: When the number of occurrences is equal to
+     * BLACK_LIST_ALARM_COUNT, the search is finished, the host reported as
+     * NOT Trustworthy, and the list of the five blacklists returned.
+     * @param ipaddress suspicious host's IP address.
+     * @return  Blacklists numbers where the given host's IP address was found.
+     */
+    public List<Integer> checkHost(String ipaddress){
+
+
+        
+        HostBlacklistsDataSourceFacade skds=HostBlacklistsDataSourceFacade.getInstance();
+
+        
+        for (int i=0;i<skds.getRegisteredServersCount() && ocurrencesCount.get() <BLACK_LIST_ALARM_COUNT;i++){
+            checkedListsCount.incrementAndGet();
+            
+            if (skds.isInBlackListServer(i, ipaddress)){
+                
+                blackListOcurrences.add(i);
+                ocurrencesCount.incrementAndGet();
+            }
+        }
+        
+        if (ocurrencesCount.get() >=BLACK_LIST_ALARM_COUNT){
+            skds.reportAsNotTrustworthy(ipaddress);
+        }
+        else{
+            skds.reportAsTrustworthy(ipaddress);
+        }                
+        
+        LOG.log(Level.INFO, "Checked Black Lists:{0} of {1}", new Object[]{checkedListsCount, skds.getRegisteredServersCount()});
+        
+        return blackListOcurrences;
+    }
+    
+    
+    public List<Integer> checkHost(String ipaddress,int cores){
+    	
+    	HostBlacklistsDataSourceFacade skds=HostBlacklistsDataSourceFacade.getInstance();
+    	int serverAmount = skds.getRegisteredServersCount();
+    	int groups = (int)Math.ceil((serverAmount/ cores));
+    	Threadblacklistvalidator[] threads = new Threadblacklistvalidator[cores];
+        for(int i =0;i<cores;i++) {
+        	threads[i] = new Threadblacklistvalidator(ipaddress,i*groups,((i+1)*groups)>serverAmount?serverAmount:(i+1)*groups,skds,ocurrencesCount,checkedListsCount);
+        }
+        long start = System.currentTimeMillis();
+        for(int i =0;i<cores;i++) {
+        	try {
+				threads[i].join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        	threads[i].start();
+        }
+
+        while(checkedListsCount.get() < serverAmount  && ocurrencesCount.get()<BLACK_LIST_ALARM_COUNT) {
+            
+        }
+        for(int i =0;i<cores;i++) {
+        	blackListOcurrences.addAll(threads[i].getBlackListOcurrences());
+        }
+        if (ocurrencesCount.get()>=BLACK_LIST_ALARM_COUNT){
+            skds.reportAsNotTrustworthy(ipaddress);
+        }
+        else{
+            skds.reportAsTrustworthy(ipaddress);
+        }                
+        
+        LOG.log(Level.INFO, "Checked Black Lists:{0} of {1}", new Object[]{checkedListsCount, skds.getRegisteredServersCount()});
+        
+        long fin = System.currentTimeMillis();
+        System.out.println("Hilos: "+cores+"   Tiempo duracion: "+(-start+fin));
+        return blackListOcurrences;
+    }
+    
+    public int getocurrencesCount() {
+    	return ocurrencesCount.intValue();
+    }
+
+	public int getcheckedlistCount() {
+		return checkedListsCount.intValue();
+	}
+	
+	public List<Integer> getblackListOcurrences(){
+		 return blackListOcurrences;
+	 }
+    
+    
+    private static final Logger LOG = Logger.getLogger(HostBlackListsValidator.class.getName());
+    
+    
+    
+}
+
+
---
+++ Lab1/src/main/java/edu/eci/arsw/blacklistvalidator/Threadblacklistvalidator.java
@@ -0,0 +1,60 @@
+package main.java.edu.eci.arsw.blacklistvalidator;
+
+import main.java.edu.eci.arsw.spamkeywordsdatasource.HostBlacklistsDataSourceFacade;
+import java.lang.Math;
+import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicInteger;
+public class Threadblacklistvalidator extends Thread{
+	private AtomicInteger ocurrencesCount=new AtomicInteger(0);
+	private AtomicInteger checkedListsCount= new AtomicInteger(0);
+	private LinkedList<Integer> blackListOcurrences = new LinkedList<>();
+	private String ipaddress;
+	private int min;
+	private int max;
+	private HostBlacklistsDataSourceFacade skds;
+
+	
+
+	Threadblacklistvalidator(String ipaddress,int min,int max,HostBlacklistsDataSourceFacade skds,AtomicInteger ocurrencesCount,AtomicInteger checkedListsCount){
+		this.min = min;
+		this.max=max;
+		this.ipaddress = ipaddress;
+		this.skds = skds;
+		this.checkedListsCount = checkedListsCount;
+		this.ocurrencesCount = ocurrencesCount;
+	}
+	public int getMax() {
+		return max;
+	}
+	public int getMin() {
+		return min;
+	}
+	public HostBlacklistsDataSourceFacade getSkds() {
+		return skds;
+	}
+	public int getCheckedListsCount() {
+		return checkedListsCount.intValue();
+	}
+	public LinkedList<Integer> getBlackListOcurrences() {
+		return blackListOcurrences;
+	}
+	public int getOcurrencesCount() {
+		return ocurrencesCount.intValue();
+	}
+	
+	public void run(){
+		for(int i=min;i<max && ocurrencesCount.get() < 5;i++) {
+			if(isInterrupted())break;
+			checkedListsCount.incrementAndGet();
+            
+            if (skds.isInBlackListServer(i, ipaddress)){
+                
+                blackListOcurrences.add(i);
+                ocurrencesCount.incrementAndGet();
+            }
+
+		}
+	}
+	
+	
+}
